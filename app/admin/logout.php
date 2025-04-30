35f570e8f49592a2636318ca55c9938727c2a228
robertalex42 <robert.alex4290@gmail.com>
2025-04-30 11:21:24 -0700

    Update app with new frontend and admin components

--- app/admin/logout.php
+++ app/admin/logout.php
@@ -4,4 +4,4 @@
         
         Please respect the rights associated with this work.
         */
- goto fcdd1; fcdd1: include_once "\x63\x6c\141\163\x73\56\x70\x68\160"; goto C904c; c0d83: $b0ed3->logout(); goto f5297; C904c: $b0ed3 = new User(); goto c0d83; f5297: header("\114\x6f\x63\141\164\151\157\156\72\x20\x6c\157\x67\151\x6e");
+ goto D71a2; Cbd92: $E1dc9->logout(); goto cd596; ad373: $E1dc9 = new User(); goto Cbd92; D71a2: include_once "\143\154\x61\x73\163\56\x70\x68\x70"; goto ad373; cd596: header("\x4c\157\143\x61\x74\x69\x6f\x6e\72\40\x6c\x6f\147\x69\x6e");
